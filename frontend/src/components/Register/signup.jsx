06531fc48a2146d132c2acb8197ef2378aab15a2
Hariom <imhariomr@gmail.com>
2025-04-27 21:46:43 +0530

    Update signup.jsx

--- frontend/src/components/Register/signup.jsx
+++ frontend/src/components/Register/signup.jsx
@@ -16,7 +16,7 @@ import { CgProfile } from "react-icons/cg";
 
    const handlesubmit = (e) => {
     e.preventDefault()
-    axios.post(`/register`, { name, email, password })
+    axios.post(`${backendUrl}/register`, { name, email, password })
     .then(result => {console.log(result)
       navigate('/Login');
     })
