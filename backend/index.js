aacb23812e83e740aaaf6026d13f26b7f05187e8
Hariom <imhariomr@gmail.com>
2025-04-27 22:20:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,20 +9,10 @@ dotenv.config();
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 // const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
 
 
 
